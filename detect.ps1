e0854c9dfd237187a212cceb775e750b6c29edf9
Mr B <iTranc3@gmail.com>
2023-03-16 11:21:56 +1000

    Add files via upload

---
+++ detect.ps1
@@ -0,0 +1,9 @@
+﻿$DriverName = "HP Universal Printing PCL 6";
+$installed = Get-PrinterDriver -Name "$DriverName" -ErrorAction SilentlyContinue;
+if($installed -eq $null){
+    Write-Host "false";
+    exit 1;
+}else{
+    Write-Host "true";
+    exit 0;
+}
